b0dad48c5bbe6d0184ce2ef9fe4c888a09a49210
Andrew Wanex <awanex2705@gmail.com>
2019-08-01 00:19:42 -0700

    refactor: change window resize event handler from hostlistener to subscription, add documentation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, AfterViewInit, OnDestroy, ChangeDetectorRef, HostListener, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { OnPageVisible, OnPageHidden } from 'angular-page-visibility';
+import { Subscription, fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { fadeInOut } from '../animations/routing-animations';
 
@@ -29,45 +31,45 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     {
       title: 'Intro', route: '/home', thumbnail: 'assets/images/intro-4.png',
       alt: 'Abstract line waves', textColor: 'light',
-      text: 'Hi,<br> I\'m Andrew,<br>a web developer'
+      text: 'Hi,<br> I\'m Andrew,<br>a web developer',
+      helpText: 'Navigate to the home page'
     }, {
       title: 'Contact', route: '/contact', thumbnail: 'assets/images/contact.jpg',
       alt: 'desk with laptop', textColor: 'dark',
-      text: 'Contact Me'
+      text: 'Contact Me',
+      helpText: 'Navigate to the contact me page'
     }, {
       title: 'About', route: '/about', thumbnail: 'assets/images/about.jpg',
       alt: 'chair in a room', textColor: 'light',
-      text: 'About Me'
+      text: 'About Me',
+      helpText: 'Navigate to the about me page'
     }, {
       title: 'Portfolio', route: '/portfolio', thumbnail: 'assets/images/portfolio.jpg',
       alt: 'white book with no title', textColor: 'dark',
-      text: 'My Projects'
+      text: 'My Projects',
+      helpText: 'Navigate to the portfolio page'
     }
   ];
   @ViewChild('carouselRingButton') carouselRingButton: ElementRef;
-  carouselRotateInterval = null;
-  imageRatios = [1, 0.8, 0.7];
-  imageWidth: number;
-  apothem = 1;
-  gapRatio = 0.1;
-  currentIndex = 0;
-  currentImage = 0;
+  carouselRotateInterval = null; // auto rotate setInterval ref
+  imageRatios = [1, 0.8, 0.7]; // adjustment ratios for different sizes of browser window
+  imageWidth: number; // image/anchor width in pixels after applying sizing ratios
+  apothem = 1; // distance from center of rotation to center of a polygon's side
+  gapRatio = 0.1; // gap between carousel images
+  currentIndex = 0; // index of carousel page
+  currentImage = 0; // image index value to calculate carousel rotation
   manualTiming = '0.5s';
   autoTiming = '1.5s';
   pauseAutoTimer = null;
-  theta = 2 * Math.PI / this.pages.length;
+  theta = 2 * Math.PI / this.pages.length; // angle of one image in carousel in radians
   figureStyle: CarouselStyle = {};
   figcaptionStyle: CarouselStyle = {};
   anchorStyle: CarouselStyle[] = [{}, {}, {}, {}];
   captionStyle: CarouselStyle[] = [{}, {}, {}, {}];
+  resizeSubscription: Subscription;
 
   constructor(private router: Router,
     public cdRef: ChangeDetectorRef) { }
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.setImageRatio(true);
-  }
 
   @OnPageVisible()
   onPageVisible(): void {
@@ -80,6 +82,12 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.resizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(500))
+      .subscribe(_ => {
+        console.log('resize');
+        this.setImageRatio(true);
+      });
     this.startAutoRotate();
   }
 
@@ -88,10 +96,18 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.resizeSubscription.unsubscribe();
     this.stopAutoRotate(true);
   }
 
-  startAutoRotate() {
+  /**
+   * Start carousel rotation timer
+   *
+   * @params: none
+   *
+   * @return: none
+  **/
+  startAutoRotate(): void {
     this.figureStyle.transition = this.autoTiming;
     if (this.carouselRotateInterval == null) {
       this.carouselRotateInterval = setInterval(() => {
@@ -100,7 +116,14 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  stopAutoRotate(onLeave?: boolean) {
+  /**
+   * Stop carousel rotate timer and set to null
+   *
+   * @params: [onLeave] - true if leaving page view
+   *
+   * @return: none
+  **/
+  stopAutoRotate(onLeave?: boolean): void {
     clearInterval(this.carouselRotateInterval);
     this.carouselRotateInterval = null;
     if (!onLeave) {
@@ -114,12 +137,27 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  rotateCarouselOnClick(event: any) {
+  /**
+   * Manually rotate the carousel when button is clicked
+   *
+   * @params: event - click event
+   *
+   * @return: none
+  **/
+  rotateCarouselOnClick(event: any): void {
     const rect = this.carouselRingButton.nativeElement.getBoundingClientRect();
     this.rotateCarousel(event.clientX < (rect.left + rect.width / 2) ? 'prev': 'next', true);
   }
 
-  rotateCarousel(direction: string, stopAutoRotate?: boolean) {
+  /**
+   * Rotate the carousel in the given direction
+   *
+   * @params: direction - either 'next' to go right or 'prev' to go left
+   * @params: [stopAutoRotate] - true if carousel should not auto rotate
+   *
+   * @return: none
+  **/
+  rotateCarousel(direction: string, stopAutoRotate?: boolean): void {
     if (stopAutoRotate) this.stopAutoRotate();
     if (direction == 'next') {
       this.currentImage++;
@@ -128,12 +166,20 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       this.currentImage--;
       this.currentIndex = !this.currentIndex ? this.pages.length - 1: this.currentIndex - 1;
     }
-    // TODO - use angular animations?
+    // rotate the carousel by current image index and its angle size
     this.figureStyle.transform = `rotateY(${this.currentImage * -this.theta}rad)`;
     this.cdRef.detectChanges();
   }
 
-  onCarouselNavigate(index: number) {
+  /**
+   * Navigate to page on carousel image click if image is front and center,
+   * otherwise rotate the carousel towards the selected index
+   *
+   * @params: index - index of clicked image
+   *
+   * @return: none
+  **/
+  onCarouselNavigate(index: number): void {
     if (this.currentIndex == index) {
       this.router.navigate([this.pages[index].route]);
     } else {
@@ -150,29 +196,46 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  setImageRatio(resize?: boolean) {
+  /**
+   * Set image width, values for 3d rotation, and image position values based
+   * on window width
+   *
+   * @params: [resize] - true if window was resized after load
+   *
+   * @return: none
+  **/
+  setImageRatio(resize?: boolean): void {
     const innerWidth = window.innerWidth;
+    // set image width with ratio from array based on window inner width
     this.imageWidth = innerWidth * this.imageRatios[(innerWidth < 599) ? 0: (innerWidth < 1199) ? 1: 2];
+    // calculate apothem for transformOrigin distance
     this.apothem = this.imageWidth / (2 * Math.tan(Math.PI / this.pages.length));
     this.figureStyle['transformOrigin'] = `50% 50% ${-this.apothem}px`;
+    // on first load, set transform to 0rad
     if (!resize) {
       this.figureStyle['transform'] = 'rotateY(0rad)';
     }
 
+    // calculate styles for anchor and caption for each page in carousel
     for (let i=0; i < this.pages.length; i++) {
+      // get the width of the image factoring in the selected gap ratio between anchors
       const width = this.gapRatio * innerWidth;
       this.anchorStyle[i] = {
         margin: `0 ${width}px`
       };
+      // set caption color based on page's text color and position based on anchor width
       this.captionStyle[i] = {
         color: `${this.pages[i].textColor == 'light' ? '#ededed' :'#090f19'}`,
         top: `${width * 0.5}px`,
         left: `${width * 1.6}px`
       };
+      // Set styling for anchors and captions other than the first (front and center)
+      // so that they start in their relative positions
       if (i) {
         this.anchorStyle[i].transformOrigin = `50% 50% ${-this.apothem}px`;
         this.anchorStyle[i].transform = `rotateY(${i * this.theta}rad)`;
         this.captionStyle[i].left = `${width * 0.7}px`
+      // First anchor is not rotated at this point
       } else {
         this.captionStyle[i].right = `${width}px`;
       }
